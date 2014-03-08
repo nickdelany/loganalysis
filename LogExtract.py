4e4bb993014e762fc630eebc08df875de5096a2e
Nick Delany <nick.delany@gmail.com>
2014-03-08 22:04:38 +0000

    Changed to filter on fixes

--- LogExtract.py
+++ LogExtract.py
@@ -4,8 +4,9 @@ import sys
 
 
 class Commit:
-    def __init__(self, commit):
+    def __init__(self, commit, message):
         self.commit = commit
+        self.message = message
         self.files = []
 
 
@@ -23,46 +24,66 @@ class LogExtract:
         self.changes = 0
         self.commits = []
         self.exclude_paths = ['test/']
-        self.file_count = 0
-
-    def update_stats(self, fname):
-        self.commits[-1].files.append(fname)
-        if fname in self.files:
-            self.files[fname].add(self.commits[-1].commit)
-        else:
-            self.files[fname] = set(self.commits[-1].commit)
-            self.file_count += 1
+        self.filter_text = ['fixed', 'fixes', 'fix']
 
     def process(self):
         for line in self.file:
             line = line.rstrip()
 
             if self.is_commit_message(line):
-                self.commits.append(Commit(line[:7]))
+                self.commits.append(Commit(line[:7], line[7:]))
                 self.changes += 1
             elif line[0:1] != 'D':  # ignore deletions
                 fname = line[2:]
                 if self.should_process_file(fname):
-                    self.update_stats(fname)
+                    self.update_file_commits(fname)
+
+    def update_file_commits(self, fname):
+        self.commits[-1].files.append(fname)
+
+        if fname not in self.files:
+            self.files[fname] = set()
+
+        self.files[fname].add(self.commits[-1].commit)
 
     def is_commit_message(self, logmsg):
         return self.commit_pattern.match(logmsg) is not None
 
     def should_process_file(self, filename):
+        # ignore non-python files and files outside the main folder.
         if not filename.endswith(".py") or not filename.startswith('django/'):
             return False
 
+        # ignore any specifically excluded paths
         for path in self.exclude_paths:
             if filename.startswith('django/' + path):
                 return False
 
         return True
 
+    def message_filter(self, msg):
+        for txt in self.filter_text:
+            if msg.lower().find(' %s ' % txt) > -1:
+                return True
+
+        return False
+
     def summary(self):
         # remove commits with no files
         self.commits = filter((lambda c: len(c.files) > 0), self.commits)
+        # remove commits not 'fixes'
+        self.commits = filter((lambda c: self.message_filter(c.message)), self.commits)
         # sort commits by number of files descending
         self.commits = sorted(self.commits, key=lambda c: len(c.files), reverse=True)
+
+        # create set of unique commit hashes
+        unique_commits = set()
+        for commits in self.commits:
+            unique_commits.add(commits.commit)
+
+        # remove any commits not in the resultant list from the file commit lists
+        for k, v in self.files.items():
+            self.files[k] = unique_commits.intersection(v)
 
         print "%d changes, %d files" % (len(self.commits), len(self.files))
 
